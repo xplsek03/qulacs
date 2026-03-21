539b863a01a7c076fcde1b0d174e496f03b3cee7
xplsek03 <michal.plsek@seznam.cz>
2026-03-21 08:41:55 +0100

    fix BK

--- src/cppsim/state_gpu.hpp
+++ src/cppsim/state_gpu.hpp
@@ -66,10 +66,6 @@ public:
             this->data(), _dim, _cuda_stream, device_number);
     }
 
-        // throw NotImplementedException(
-        //     "set_zero_norm_state for QuantumStateGpu is not implemented "
-        //     "yet");
-    }
     /**
      * \~japanese-en 量子状態を<code>comp_basis</code>の基底状態に初期化する
      *
